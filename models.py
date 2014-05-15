b936350cdb9fbddc82a78240658e2611105ee9c5
EvilDmitri <dmitribrach@gmail.com>
2014-05-15 15:16:27 +0300

    user 'scraper'

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from time import gmtime, strftime
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, orm
 
-engine = create_engine('mysql://localhost/scraper_data')
+engine = create_engine('mysql://scraper@localhost/scraper_data')
 
 meta = MetaData()
 
